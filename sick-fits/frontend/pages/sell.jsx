ccb37f1839a5a407117a390d2d0188e916c8291e
Daniel Zandi <daniel.zandi@upnetix.com>
2020-01-14 14:45:43 +0200

    creating Items and mutations and uploading images to cloudinary

--- sick-fits/frontend/pages/sell.jsx
+++ sick-fits/frontend/pages/sell.jsx
@@ -1,9 +1,8 @@
-import Link from 'next/link';
+import CreateItem from '../components/CreateItem';
 
-class Sell extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Sell = props => {
+  return <CreateItem />
+};
+
 
 export default Sell;
