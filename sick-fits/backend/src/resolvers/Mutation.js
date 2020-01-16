417b0b3abe4ab96966b9e8334dfcb1254552726e
Daniel Zandi <daniel.zandi@upnetix.com>
2020-01-16 10:38:58 +0200

    add delete function

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,10 +8,31 @@ const Mutations = {
     }, info);
     
     return item;
-  }
+  },
   // createDog(parent, args, ctx, info) {
   //   console.log(args);
   // }
+  updateItem(parent, args, ctx, info) {
+    // first take copy of updates
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info);
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`)
+    // 2. check if they own that item, or have the permissions
+    // TO DO
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
