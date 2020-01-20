358213aea62f2b91dff85b511da02cb7be6d7a32
Daniel Zandi <daniel.zandi@upnetix.com>
2020-01-20 17:17:13 +0200

    user sign up permisiion flow, user signup in reack, currently logged user

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -2,10 +2,22 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const server = createServer();
 
 server.express.use(cookieParser());
+
+// decode the JWT so we can get the user id on each request
+server.express.use((req, res, next)=> {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start({
   cors: {
